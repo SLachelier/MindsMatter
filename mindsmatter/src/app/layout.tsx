6ed12aaca48bdb7b27f40ab385d565a3213d2629
Shauna L <slachelier@gmail.com>
2024-02-10 17:51:46 -0600

    initialized next.js and dependencies

---
+++ mindsmatter/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "MindsMatter",
+  description: "A mental health app that provides an AI chatbot and resources to help you manage your mental health.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
